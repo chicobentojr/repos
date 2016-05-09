ce553629f880a8f5f3f5faa6225afa8b7e0cb5a1
chicobentojr <francisco.bento.jr@hotmail.com>
2016-05-08 23:11:19 -0300

    Add languages to search repo

--- src/app/repos/repos.controller.js
+++ src/app/repos/repos.controller.js
@@ -11,17 +11,25 @@
 
     vm.user = 'chicobentojr'
     vm.repos = [];
+    vm.languages = [];
     vm.loading = true
-    vm.search = null;
+    vm.search = '';
 
     githubRepos.getRepos(vm.user, function(response){
       vm.repos = response;
       vm.loading = false;
+      response.forEach(function(item, index){
+        if (item.language && $.inArray(item.language, vm.languages) == -1){
+          vm.languages.push(item.language);
+        }
+      });
+      $('.form .dropdown').dropdown({allowAdditions: true});
     });
 
     vm.searchRepos = function(repo){
-      var keyword = new RegExp(vm.search, 'i');
-      return !vm.search || keyword.test(repo.name) || keyword.test(repo.language);
+        if(vm.search == -1) return true;
+        var keyword = new RegExp(vm.search, 'i');
+        return !vm.search || keyword.test(repo.name) || keyword.test(repo.language);
     }
   }
 })();
